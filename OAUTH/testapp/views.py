c267dca7efb8ee163be6cae57adc8cda0fb1719f
himavardhank <himavardhank@gmail.com>
2019-02-07 21:08:38 +0530

    all prj

---
+++ OAUTH/testapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from testapp.models import Employee
+from testapp.serializers import EmployeeSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.authentication import TokenAuthentication
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import BasicAuthentication
+class Employee_CRUD(ModelViewSet):
+    serializer_class=EmployeeSerializer
+    queryset=Employee.objects.all()
+    authentication_classes=[BasicAuthentication,]
+    permission_classes=[IsAuthenticated,]
+
+
+# Create your views here.
